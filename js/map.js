3bf18e11d3ad72b4f9023592bd1fce56617e0919
Eric Nolte <amishe@gmail.com>
2014-09-18 13:36:54 -0600

    refactor map into factory and wrap up launching code into anon function

--- js/map.js
+++ js/map.js
@@ -1,117 +1,103 @@
 
+// Insert your Firebase database here //
 var database = new Firebase(
-// Insert your database URL here//
+"https://amber-fire-3032.firebaseio.com/demoDB"
 );
-var mapStyle = [
-  {
-    stylers: [
-      { hue: "#00ffe6" },
-      { saturation: -20 }
-    ]
-  },{
-    featureType: "road",
-    elementType: "geometry",
-    stylers: [
-      { lightness: 100 },
-      { visibility: "simplified" }
-    ]
-  },{
-    featureType: "road",
-    elementType: "labels",
-    stylers: [
-      { visibility: "off" }
-    ]
-  }
-];
 
+// Change your map form id if necessary //
+var form = $('#placeForm')
 
-var form = $('#placeForm');
-
-var mapCenterCoord = { 
+// Play with different map attributes here, change your canvas id if different //
+var mapVars = {
+  
+  style : [
+    {
+      stylers: [
+        { hue: "#00ffe6" },
+        { saturation: -20 }
+      ]
+    },{
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { lightness: 100 },
+        { visibility: "simplified" }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    }
+  ],
+  canvas : $('#map-canvas')[0],
+  centerCoord : { 
   // Nice center point over North America //
   lat:39.00, 
   lng:-100.00 
+  },
+  zoomLevel:4
 };
 
-var mapZoomLevel = 4;
-
-var mapCenterPoint = new google.maps.LatLng(mapCenterCoord.lat, mapCenterCoord.lng);
-
-var mapOptions =  {
-  center: mapCenterPoint,
-  zoom: mapZoomLevel,
-}; 
-
-var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-map.setOptions({styles: mapStyle});
-
-// Populates map at load and retrieves new entries in real-time //
-database.on('child_added', function(snapshot) {
-  var entry = snapshot.val();
-  newMarker = new Marker(entry, map);
-  newMarker.setMarker();
-});
+// Thats it!  The rest shouldn't need any modifications. //
 
 
 
-//Fires a DB push on form submit and hides form//
-form.submit(function( event ) {
-  var Submitter = $('#submitter').val();
-  var Comments = $('#commentsField').val();
-  var place = autocomplete.getPlace();
-  form.hide();
-  confirmSubmission();
-  event.preventDefault();
-  database.push({
-                      name:place.name,
-                      lat:place.geometry.location.k,
-                      long:place.geometry.location.B,
-                      submitter:Submitter,
-                      comments:Comments });
-});
 
-function confirmSubmission() {
-  var Confirmation = $('#confirmation');
-  Confirmation.text('Thank you for your submission!');
+var Map = function (mapVars) {
+  var mapCenterPoint = new google.maps.LatLng(
+    mapVars.centerCoord.lat, 
+    mapVars.centerCoord.lng
+  );
+
+  var mapOptions = {
+    center: mapCenterPoint,
+    zoom: mapVars.zoomLevel,
+  }; 
+  
+  var map = new google.maps.Map(mapVars.canvas, mapOptions);
+  map.setOptions({styles: mapVars.style});
+  
+  return map;
 };
 
-// Constructor for map markers and info windows //
+
 var Marker = function(entry, map) {
 
   this.setMarker = function () {
-    var newMarker = this.marker();
-    var newInfoWindow = this.infoWindow();
-    this.addMarkerListener(newMarker, newInfoWindow);
+    var newMarker = marker();
+    var newInfoWindow = infoWindow();
+    addMarkerListener(newMarker, newInfoWindow);
     newMarker.setMap(map);
   };
 
-  this.addMarkerListener = function (marker, infoWindow) {
+  addMarkerListener = function (marker, infoWindow) {
       google.maps.event.addListener(marker, 'click', function() {
         infoWindow.open(map,marker);
       });
   };
 
-  this.marker = function () {
+  marker = function () {
     return new google.maps.Marker({
-      position: this.latLong(),
+      position: latLong(),
       map: map,
       title: entry.name,
       animation: google.maps.Animation.DROP
     });
   };
 
-  this.latLong = function () {
+  latLong = function () {
     return new google.maps.LatLng(entry.lat, entry.long); 
   };
 
-  this.infoWindow = function () {
-    var infoWindowContent = this.contentHtml();
+  infoWindow = function () {
+    var infoWindowContent = contentHtml();
     return new google.maps.InfoWindow({content: infoWindowContent});
   };
 
 
-  this.contentHtml = function() {
+  contentHtml = function() {
     return '<div id="content">'+
     '<div id="siteNotice">'+
     '</div>'+
@@ -123,4 +109,36 @@ var Marker = function(entry, map) {
     '</div>';
   };
 };
+
+
+(function () {
+  var map = Map(mapVars);
+  database.on('child_added', function(snapshot) {
+    var entry = snapshot.val();
+    newMarker = new Marker(entry, map);
+    newMarker.setMarker();
+  });
+
+  form.submit(function( event ) {
+    var Submitter = $('#submitter').val();
+    var Comments = $('#commentsField').val();
+    var place = autocomplete.getPlace();
+    form.hide();
+    confirmSubmission();
+    event.preventDefault();
+    database.push({
+                        name:place.name,
+                        lat:place.geometry.location.k,
+                        long:place.geometry.location.B,
+                        submitter:Submitter,
+                        comments:Comments });
+  });
+
+  function confirmSubmission() {
+    var Confirmation = $('#confirmation');
+    Confirmation.text('Thank you for your submission!');
+  };
+})(); 
+
+
 
